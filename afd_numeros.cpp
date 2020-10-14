569e080cd714a4be1255f86ff01d18aa1932ed05
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2020-10-14 10:00:31 -0300

    Update afd_numeros.cpp

--- afd_numeros.cpp
+++ afd_numeros.cpp
@@ -21,41 +21,43 @@ int main(){
 	cout<<endl<<"*****=====*****=====*****=====*****";
 	
 	int i,j,achou = 0;
-	char fita[100];
+	char fita[100]; //Vetor de entrada
 	int atual,final;     
 	
+	//Matriz estática
 	char m[3][3]={{'0', '1', 'v'},
 			      {'0', 'v', '1'},
 				  {'v', '1', '0'}};
 						
-	atual = 0;
-	final = 1;
+	atual = 0; //Estado inicial
+	final = 1; //Estado final
 	
+	//Cadeia de entrada  
 	cout<<"\n\nSentencas permitidas(0 ou 1): " ;
 	cin>>fita;
-	cout<<endl<<"Entrada: "<<fita;
+	cout<<endl<<"Entrada: "<<fita; //Exibir cadeia
  
-  i=0;
+  i=0;//Contador
   while(fita[i] != '\0')
   {
                    achou = 0;
-                   for(j=0;j<3;j++)
+                   for(j=0;j<3;j++) //Percorrendo a matriz
                    {
-                     if( m[atual][j]==fita [i])
+                     if( m[atual][j]==fita [i]) //Se o elemento da matriz for igual ao da entrada
                      {
-                       atual=j;
-                       i++;
-                       j=3;
+                       atual=j; //Atual recebe a posição da matriz
+                       i++; //Incrementa 1 ao índice da entrada(passa para o valor seguinte) 
+                       j=3; 
                        achou =1;
                      }
                             
                    }
                    if(achou == 0  )
                    {
-                         i='\0';
+                         i='\0'; //Se a variável "achou" for igual a zero é pq a entrada é nula.
                    } 
   }
-  if(atual==final)
+  if(atual==final) //Condicional para determinar se o último elemento é igual ao estado final
   {
     cout<<endl<<"A CADEIA FOI RECONHECIDA!\n";
                    
